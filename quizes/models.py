40d62bed0057264789097711ae86435f10bc1ee6
Liyuraz1 <dev.leeyu@gmail.com>
2022-10-22 15:00:52 +0100

    updated

---
+++ quizes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+DIFF_CHOICES = (
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    required_score_to_pass = models.IntegerField(help_text="required_score to pass in %")
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_questions(self):
+        return self.question_set.all()[:self.number_of_questions]
+
+    class Meta:
+        verbose_name_plural = 'Quizes'
